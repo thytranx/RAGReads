5e7f96387f36ea6a9a725bee13d4690f84d19f78
GORLEABHILASH <36183586+GORLEABHILASH@users.noreply.github.com>
2025-03-09 17:48:36 -0400

    RAG Code

--- RAG.py
+++ RAG.py
@@ -14,7 +14,7 @@ from sentence_transformers import SentenceTransformer
 load_dotenv()
 
 # Neo4j connection
-NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_URI = os.getenv("NEO4J_URI", "")
 NEO4J_USER = os.getenv("NEO4J_USER", "")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
